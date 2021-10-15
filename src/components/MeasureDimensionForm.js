fb80b7c9ec4631e78cadba872cc6a7839e5f8390
khaled-Muhamed <khaled.m.abdelghany@gmail.com>
2021-10-15 22:52:38 +0200

    Make API call Dynamic built on Client input

--- src/components/MeasureDimensionForm.js
+++ src/components/MeasureDimensionForm.js
@@ -1,14 +1,33 @@
-import AddDimension from './AddDimension'
-import AddMeasure from './AddMeasure'
-import Button from './Button'
+import { useState } from "react"
 
 const MeasureDimensionForm = ({getData}) => {
+    const [dimension ,setDimension] = useState('')
+    const [measure ,setMeasure] = useState('')
+
+    const onSubmit = (e)=>{
+        e.preventDefault()
+        //validate here the dimension and measure before calling the Api
+        //if all is ok call the API and clear fields
+        getData(measure,dimension)
+        setDimension('')
+        setMeasure('')
+        
+    }
     return (
-        <div>
-            <AddDimension />
-            <AddMeasure />
-            <Button color='green' text='Hello' getData = {getData}/>
-        </div>
+        <form>
+            <div>
+                <label>Dimension</label>
+                <input type="text" placeholder="Enter a Dimension" 
+                value={dimension} onChange={(e)=>setDimension(e.target.value)}></input>
+            </div>
+            <div>
+                <label>Measure</label>
+                <input type="text" placeholder="Enter a Measure"
+                value={measure} onChange={(e)=>setMeasure(e.target.value)}></input>
+            </div> 
+            {/* <input className="btn btn-block" type="submit" value="Build Plot" onSubmit={onSubmit}/> */}
+            <button onClick={onSubmit}>Build Plot</button>
+        </form>
     )
 }
 
